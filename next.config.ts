c53ee64ac0078d26774f9cee364ba845f7a24d43
unknown <fsang@umich.edu>
2026-06-28 12:39:57 -0400

    Initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
+import type { Configuration } from 'webpack'
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  /* handle 3D model files */
+  webpack: (config: Configuration) => {
+    config.module.rules.push({
+      test: /\.(glb|gltf)$/,
+      use: { loader: 'file-loader' }
+    });
+    return config;
+  }
 };
 
 export default nextConfig;
